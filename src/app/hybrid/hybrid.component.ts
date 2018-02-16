70642966c896004f27f632103509fbd4600d3f97
Ravikumar Govindaraj <rgovindaraj@vmware.com>
2018-02-16 08:57:37 +0530

    Hybrid HA error correction

--- src/app/hybrid/hybrid.component.ts
+++ src/app/hybrid/hybrid.component.ts
@@ -14,119 +14,114 @@ export class HybridComponent implements OnInit {
 
 
   usableSpace() {
-            
-    this.canvas.width = 500;
-    this.canvas.height = 500;
     
-    if (this.hybrid.newPieChart) {
-      this.hybrid.newPieChart.destroy();
-    }
+this.canvas.width = 500;
+this.canvas.height = 500;
 
-                  this.hybrid.effectiveSpace = ((this.hybrid.totalNodes * 
-                                                    this.hybrid.totalDiskGroups * 
-                                                    this.hybrid.disksPerDiskGroup * 
-                                                    this.hybrid.value) / 
-                                                    (this.hybrid.FTTvalue)/ 
-                                                    (this.hybrid.inTerabytes)*
-                                                    (1-((this.hybrid.slackSpace)/100))).toFixed(2);
-                 
-                  if (isNaN(this.hybrid.effectiveSpace)) {
-                    this.hybrid.effectiveSpace = 0;
-                  } 
+if (this.hybrid.newPieChart) {
+this.hybrid.newPieChart.destroy();
+}
+
+          this.hybrid.effectiveSpace = ((this.hybrid.totalNodes * 
+                                            this.hybrid.totalDiskGroups * 
+                                            this.hybrid.disksPerDiskGroup * 
+                                            this.hybrid.value) / 
+                                            (this.hybrid.FTTvalue)/ 
+                                            (this.hybrid.inTerabytes)*
+                                            (1-((this.hybrid.slackSpace)/100))).toFixed(2);
+
+          if (isNaN(this.hybrid.effectiveSpace)) {
+            this.hybrid.effectiveSpace = 0;
+          }  
+
+          this.hybrid.chartEF = ((this.hybrid.effectiveSpace)*1000).toFixed(0);
 
 
-                  this.hybrid.chartEF = ((this.hybrid.effectiveSpace)*1000).toFixed(0);
-
-
-                  this.hybrid.chartSlack = (( this.hybrid.totalNodes * 
-                                                      this.hybrid.totalDiskGroups * 
-                                                      this.hybrid.disksPerDiskGroup * 
-                                                      this.hybrid.value)*
-                                                      (1.0 - this.hybrid.chartSlackspacevalue)).toFixed(0);
-                  
-                  this.hybrid.chartRF = ((((this.hybrid.effectiveSpace)*
+          this.hybrid.chartSlack = ((this.hybrid.totalNodes * 
+                                    this.hybrid.totalDiskGroups * 
+                                    this.hybrid.disksPerDiskGroup * 
+                                    this.hybrid.value)*
+                                    ((this.hybrid.slackSpace)/100)).toFixed(0);
+          
+          this.hybrid.chartRF = ((((this.hybrid.effectiveSpace)*
                                     (this.hybrid.FTTvalue))-
-                                    (this.hybrid.effectiveSpace))*1000).toFixed(0);  
-                  
-                  this.hybrid.chartFsUtil = ((this.hybrid.totalNodes * 
-                                                this.hybrid.totalDiskGroups * 
-                                                this.hybrid.disksPerDiskGroup * 
-                                                this.hybrid.value)*.01).toFixed(0);
-                                                
-                  this.hybrid.rawCapacity = (((this.hybrid.totalNodes * 
-                                                  this.hybrid.totalDiskGroups * 
-                                                  this.hybrid.disksPerDiskGroup * 
-                                                  this.hybrid.value))/1000).toFixed(2);
-                    
+                                    (this.hybrid.effectiveSpace))*1000).toFixed(0); 
+                            
+          this.hybrid.chartFsUtil = ((this.hybrid.totalNodes * 
+                                      this.hybrid.totalDiskGroups * 
+                                      this.hybrid.disksPerDiskGroup * 
+                                      this.hybrid.value)*.01).toFixed(0);
+          
+          this.hybrid.rawCapacity = (((this.hybrid.totalNodes * 
+                                        this.hybrid.totalDiskGroups * 
+                                        this.hybrid.disksPerDiskGroup * 
+                                        this.hybrid.value))/1000).toFixed(2);
+          
 
-    this.hybrid.values = [this.hybrid.chartEF, this.hybrid.chartRF, this.hybrid.chartSlack, this.hybrid.chartFsUtil ];
-    
-    this.hybrid.newPieChart = new Chart(this.ctx, {
-      type: 'pie',
-      data: {
-          labels: ["Workload space", "Replica or Parity", "HA and Maintenance", "Filesystem"],
-          datasets: [{
-              label: 'vSAN Space',
-              data: this.hybrid.values,
-              backgroundColor: [
-                'rgba(46, 204, 113,   1)',
-                'rgba(231, 76, 60, 1)',
-                'rgba(142, 68, 173, 1)',
-                'rgba(243, 156, 18, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
+this.hybrid.values = [this.hybrid.chartEF, this.hybrid.chartRF, this.hybrid.chartSlack, this.hybrid.chartFsUtil ];
+
+this.hybrid.newPieChart = new Chart(this.ctx, {
+type: 'pie',
+data: {
+  labels: ["Workload space", "Replica or Parity", "HA and Maintenance", "Filesystem"],
+  datasets: [{
+      label: 'vSAN Space',
+      data: this.hybrid.values,
+      backgroundColor: [
+        'rgba(46, 204, 113, 1)',
+        'rgba(231, 76, 60, 1)',
+        'rgba(142, 68, 173, 1)',
+        'rgba(243, 156, 18, 1)'
+      ],
+      borderWidth: 1
+  }]
+},
+options: {
+cutoutPercentage: 50,
+responsive: true,
+display:true
+}
+}); 
+}
+
+
+ngOnInit() {    
+
+}
+
+
+
+canvas: any;
+ctx: any;
+
+ngAfterViewInit() {
+
+this.canvas = document.getElementById('myChart');
+this.ctx = this.canvas.getContext('2d');
+
+let myChart = new Chart(this.ctx, {
+type: 'pie',
+data: {
+    labels: ["Workload space", "Replica or Parity", "HA and Maintenance", "Filesystem"],
+    datasets: [{
+        label: 'vSAN Space',
+        data: [this.hybrid.chartEF, this.hybrid.chartRF, this.hybrid.chartSlack, this.hybrid.chartFsUtil ],
+        backgroundColor: [
+            'rgba(46, 204, 113,   1)',
+            'rgba(231, 76, 60, 1)',
+            'rgba(142, 68, 173, 1)',
+            'rgba(243, 156, 18, 1)'
+        ],
+            borderWidth: 1
+        }]
+    },
       options: {
-        cutoutPercentage: 50,
-        responsive: true,
-        display:true
-      }
-      
-    }); 
-    
-   
-  }
-
-
-  ngOnInit() {    
-
-  }
-  
-  
- 
-  canvas: any;
-     ctx: any;
- 
-     ngAfterViewInit() {
-
-      this.canvas = document.getElementById('myChart');
-      this.ctx = this.canvas.getContext('2d');
-
-      let myChart = new Chart(this.ctx, {
-        type: 'pie',
-        data: {
-            labels: ["Workload space", "Replica or Parity", "HA and Maintenance", "Filesystem"],
-            datasets: [{
-                label: 'vSAN Space',
-                data: [this.hybrid.chartEF, this.hybrid.chartRF, this.hybrid.chartSlack, this.hybrid.chartFsUtil ],
-                backgroundColor: [
-                    'rgba(46, 204, 113,   1)',
-                    'rgba(231, 76, 60, 1)',
-                    'rgba(142, 68, 173, 1)',
-                    'rgba(243, 156, 18, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-          cutoutPercentage: 50,
-          responsive: true,
-          display:true 
-        }
-        
-      });
+      cutoutPercentage: 50,
+      responsive: true,
+      display:true
     }
+  }); 
+}
         
    
 }
